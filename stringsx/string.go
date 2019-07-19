3dd96b83244eb5af6b28210ef477f07a106fdfc2
aikaiqiang <akq930210@126.com>
2019-07-19 12:32:27 +0800

    大版本更新

--- stringsx/string.go
+++ stringsx/string.go
@@ -2,6 +2,19 @@ package stringsx
 
 import "fmt"
 
-func Hello(name string) string{
-	return fmt.Sprintf("Hello, %s", name)
+func Hello(name string) (string, error) {
+	return fmt.Sprintf("Hello, %s", name), nil
+}
+
+func HelloByLang(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "zh":
+		return fmt.Sprintf("你好, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return fmt.Sprintf("unknow , %s!", name), nil
+	}
 }

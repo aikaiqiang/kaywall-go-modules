d84e13d50f040060a0070d3035e4548e9c276db6
aikaiqiang <akqdream@gmail.com>
2019-12-24 19:11:26 +0800

    [Update] go update

--- src/7-file/file.go
+++ src/7-file/file.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"os"
+	"strconv"
+	"syscall"
+)
+
+func main() {
+	filename := "template/"
+	for try := 0; try < 2; try++ {
+		file, err := os.Create(filename + strconv.Itoa(try))
+		defer file.Close()
+		if err == nil {
+			return
+		}
+		if e, ok := err.(*os.PathError); ok && e.Err == syscall.ENOSPC {
+			//deleteTempFiles()  // Recover some space.
+			//data, err := ioutil.ReadAll(file)
+			//if err != nil {
+			//	fmt.Printf("11-error: %v\n", err)
+			//	return
+			//}
+			//fmt.Printf(string(data))
+			continue
+		}
+		return
+	}
+}
